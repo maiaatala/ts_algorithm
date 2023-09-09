4ad5c3c2fdf2bbe06e5c8df34148e08c4452ab18
maiaatala <ana.atala@unemat.br>
2023-09-09 13:10:42 -0400

    chore: change to bun

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,8 @@
 import { someSum } from '~utils';
+import { expect, describe, test } from 'bun:test';
 
 describe('blah', () => {
-  it('sum', () => {
+  test('sum', () => {
     const result = someSum(2, 3);
 
     expect(result).toEqual(5);
