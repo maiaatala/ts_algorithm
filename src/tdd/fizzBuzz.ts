cddac6f23cb88803cdf682531667921c61a4cc54
maiaatala <ana.atala@unemat.br>
2024-05-01 22:48:55 -0400

    tdd6: write code required for buzz

--- src/tdd/fizzBuzz.ts
+++ src/tdd/fizzBuzz.ts
@@ -2,11 +2,16 @@ export const fizzBuzz = (N: number): (string | number)[] => {
   const resultArr = [];
 
   for (let i = 1; i <= N; i++) {
-    let currResult: string | number = i;
     if (i % 3 === 0) {
-      currResult = 'Fizz';
+      resultArr.push('Fizz');
+      continue;
     }
-    resultArr.push(currResult);
+
+    if (i % 5 === 0) {
+      resultArr.push('Buzz');
+      continue;
+    }
+    resultArr.push(i);
   }
 
   return resultArr;
